34c30f3fa4abd875ac2ac08edb5381d1454c7507
majesiu <pawel.majewski@mail.com>
2019-08-24 22:54:33 +0200

    improved help command

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,38 @@
+const { prefix } = require('../config.json');
+
 module.exports = {
 	name: 'help',
-	aliases: ['h', 'hlp'],
-	description: 'Returns help message',
+	description: 'List all of my commands or info about a specific command.',
+	aliases: ['commands', 'h'],
+	usage: '[command name]',
 	cooldown: 5,
 	execute(message, args, client) {
-		message.channel.send('```!st or !standings show current standings, add m e.g. !st m to see MiLPBE Standings\n!p Player Name show currents stats```');
+		const data = [];
+		const { commands } = message.client;
+
+		if (!args.length) {
+			data.push('Here\'s a list of all my commands: ');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+
+			return message.channel.send(data);
+		}
+
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return message.reply('that\'s not a valid command!');
+		}
+
+		data.push(`**Name:** ${command.name}`);
+
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+		message.channel.send(data, { split: true });
 	},
 };
