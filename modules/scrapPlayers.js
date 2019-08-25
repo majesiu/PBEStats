7c4c9e5c014154a3c9b2657a32f75bdac30b8aa7
majesiu <pawel.majewski@mail.com>
2019-08-25 17:24:12 +0200

    Changed the source pages for player list to include retired players

--- modules/scrapPlayers.js
+++ modules/scrapPlayers.js
@@ -8,8 +8,52 @@ const players = {};
 
 function initializePlayersList() {
 	let letter = 'a';
+	// for (let i = 0; i < 26; i++) {
+	// 	http.get(`http://www.pbesim.com/leagues/league_100_players_${letter}.html`, (resp) => {
+	// 		let data = '';
+
+	// 		resp.on('data', (chunk) => {
+	// 			data += chunk;
+	// 		});
+
+	// 		resp.on('end', () => {
+	// 			$('td .dl > a[href*="player"]', data).each(function(index, element) {
+	// 				const key = $(element).text().split(',');
+	// 				const id = $(element).attr('href').match(/\d+/)[0];
+	// 				players[(`${key[1].toLowerCase().trim()} ${key[0].toLowerCase().trim()}`).trim()] = id;
+	// 			});
+	// 		});
+
+	// 	}).on('error', (err) => {
+	// 		console.log('Error: ' + err.message);
+	// 	});
+	// 	letter = nextChar(letter);
+	// }
+	// letter = 'a';
+	// for (let i = 0; i < 26; i++) {
+	// 	http.get(`http://www.pbesim.com/leagues/league_101_players_${letter}.html`, (resp) => {
+	// 		let data = '';
+
+	// 		resp.on('data', (chunk) => {
+	// 			data += chunk;
+	// 		});
+
+	// 		resp.on('end', () => {
+	// 			$('td .dl > a[href*="player"]', data).each(function(index, element) {
+	// 				const key = $(element).text().split(',');
+	// 				const id = $(element).attr('href').match(/\d+/)[0];
+	// 				players[(`${key[1].toLowerCase().trim()} ${key[0].toLowerCase().trim()}`).trim()] = id;
+	// 			});
+	// 		});
+
+	// 	}).on('error', (err) => {
+	// 		console.log('Error: ' + err.message);
+	// 	});
+	// 	letter = nextChar(letter);
+	// }
+	letter = 'a';
 	for (let i = 0; i < 26; i++) {
-		http.get(`http://www.pbesim.com/leagues/league_100_players_${letter}.html`, (resp) => {
+		http.get(`http://www.pbesim.com/history/league_100_players_by_letter_${letter}.html`, (resp) => {
 			let data = '';
 
 			resp.on('data', (chunk) => {
@@ -18,9 +62,9 @@ function initializePlayersList() {
 
 			resp.on('end', () => {
 				$('td .dl > a[href*="player"]', data).each(function(index, element) {
-					const key = $(element).text().split(',');
-					const id = $(element).attr('href').match(/\d+/)[0];
-					players[(`${key[1].toLowerCase().trim()} ${key[0].toLowerCase().trim()}`).trim()] = id;
+					const key = $(element).text().toLowerCase().trim();
+					const id = $(element).attr('href').match(/\d+/)[0];  
+					players[key] = id;
 				});
 			});
 
@@ -31,7 +75,7 @@ function initializePlayersList() {
 	}
 	letter = 'a';
 	for (let i = 0; i < 26; i++) {
-		http.get(`http://www.pbesim.com/leagues/league_101_players_${letter}.html`, (resp) => {
+		http.get(`http://www.pbesim.com/history/league_101_players_by_letter_${letter}.html`, (resp) => {
 			let data = '';
 
 			resp.on('data', (chunk) => {
@@ -40,9 +84,9 @@ function initializePlayersList() {
 
 			resp.on('end', () => {
 				$('td .dl > a[href*="player"]', data).each(function(index, element) {
-					const key = $(element).text().split(',');
-					const id = $(element).attr('href').match(/\d+/)[0];
-					players[(`${key[1].toLowerCase().trim()} ${key[0].toLowerCase().trim()}`).trim()] = id;
+					const key = $(element).text().toLowerCase().trim();
+					const id = $(element).attr('href').match(/\d+/)[0];  
+					players[key] = id;
 				});
 			});
 
