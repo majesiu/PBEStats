3fd402abe43c7554695e462940085737e190185b
majesiu <pawel.majewski@mail.com>
2019-09-01 14:25:00 +0200

    array created just once

--- modules/scrapPlayers.js
+++ modules/scrapPlayers.js
@@ -2,17 +2,17 @@
 * MIT License
 *
 * Copyright (c) 2019 Paweł Majewski
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,6 +28,7 @@ function nextChar(c) {
 	return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 const players = {};
+const playerNames = [];
 
 function initializePlayersList() {
 	let letter = 'a';
@@ -137,12 +138,13 @@ function getPlayers() {
 }
 
 function getPlayersNames() {
-	const playerNames = [];
-	for (const key in players) {
-		if (Object.prototype.hasOwnProperty.call(players, key)) {
-			const name = new Object();
-			name.fullName = key;
-			playerNames.push(name);
+	if(playerNames.length === 0) {
+		for (const key in players) {
+			if (Object.prototype.hasOwnProperty.call(players, key)) {
+				const name = new Object();
+				name.fullName = key;
+				playerNames.push(name);
+			}
 		}
 	}
 	return playerNames;
