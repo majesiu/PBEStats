b18d9f11ee75a191763ac19fa094bce7161c03d4
majesiu <pawel.majewski@mail.com>
2019-09-06 22:54:09 +0200

    Improved leadearboards

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,17 +2,17 @@
 * MIT License
 *
 * Copyright (c) 2019 Paweł Majewski
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,6 +32,8 @@ module.exports = {
 	cooldown: 5,
 	aliases: ['leaderboard', 'lead', 'top'],
 	execute(message, args, client) {
+		const pitchersMode = args[args.length - 1] === 'p' ? true : false;
+		if (pitchersMode) args.pop();
 		const minorsMode = args[args.length - 1] === 'm' ? true : false;
 		if (minorsMode) args.pop();
 		const stat = args.join(' ');
@@ -51,12 +53,9 @@ module.exports = {
 						},
 						title: minorsMode ? 'MiLPBE Leaders' : 'PBE Leaders',
 						url: minorsMode ? 'http://www.pbesim.com/leagues/league_101_stats.html' : 'http://www.pbesim.com/leagues/league_100_stats.html',
-						thumbnail: {
-							url: 'http://www.pbesim.com/images/league_logos/pro_baseball_experience.png',
-						},
 						fields: [{
-							name: `${stat} Top 5 Leaderboard. Remember to type stat name, only stats from stats index page avalaible`,
-							value:  getStatsInfo(data, stat),
+							name: `${stat.toUpperCase()} Top 5 Leaderboard. Add m at the end for minors, add p for pitchers in case of doubled stats e.g. \`!lead WAR m p\``,
+							value:  getStatsInfo(data, stat, pitchersMode),
 							inline: true,
 						}],
 						timestamp: new Date(),
@@ -86,15 +85,16 @@ const config = {
 	},
 };
 
-function getStatsInfo(data, stat) {
+function getStatsInfo(data, stat, pitchersMode) {
 	const resultTable = [];
+	let skipCount = 0;
 	const set = $(`th:icontains(${stat})`, data).parent().parent().children();
-	console.log(set.length);
 	if(set.length >= 6) {
-		for (let i = 0; i < 6; i++) {
+		if(set.length >= 12 && pitchersMode) skipCount = 6;
+		for (let i = 0 + skipCount; i < 6 + skipCount; i++) {
 			const lineChilds = set.eq(i).children();
-			if (i === 0) resultTable.push(['#', [lineChilds.eq(0).text().trim()], 'Team', '']);
-			else resultTable.push([`${i}.`, lineChilds.eq(0).text().trim(), lineChilds.eq(1).text().trim(), lineChilds.eq(2).text().trim()]);
+			if (i - skipCount === 0) resultTable.push(['#', [lineChilds.eq(0).text().trim()], 'Team', '']);
+			else resultTable.push([`${i - skipCount}.`, lineChilds.eq(0).text().trim(), lineChilds.eq(1).text().trim(), lineChilds.eq(2).text().trim()]);
 		}
 		return '```CSS\n' + table(resultTable, config) + '```';
 	}
