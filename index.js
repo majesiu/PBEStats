e73b1f542a922f3a51893bd64ae9eca3f589954f
majesiu <pawel.majewski@mail.com>
2020-03-08 21:20:32 +0100

    fixed brace

--- index.js
+++ index.js
@@ -118,8 +118,7 @@ client.on('message', message => {
 
 	try {
 		command.execute(message, args, client);
-	}
-	catch (error) {
+	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
