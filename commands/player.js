7f195130115bfdabefa72fd438b9cdf26ceeebec
Paweł Majewski <pawel.majewski@mail.com>
2020-10-07 13:45:10 +0200

    Season 21

--- commands/player.js
+++ commands/player.js
@@ -158,7 +158,7 @@ function parseAdvancedBattingStats(data) {
 
 function parseFieldingStats(data) {
 	let fieldingInfo = '\n**Fielding**';
-	const seasonYear = 2036;
+	const seasonYear = 2037;
 	const set = $(`a[href*="team_year"]:contains(${seasonYear}):contains(- MLB)`, data).parent().parent();
 	const setMinors = $(`a[href*="team_year"]:contains(${seasonYear}):contains(- R)`, data).parent().parent();
 	for (let i = 0; i < set.length; i++) {
