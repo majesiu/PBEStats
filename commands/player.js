b929a12a3fd3f0926927bbefab188f8c3d0722b5
Paweł Majewski <pawel.majewski@mail.com>
2023-04-05 10:52:29 +0200

    Season 39

--- commands/player.js
+++ commands/player.js
@@ -162,7 +162,7 @@ function parseAdvancedBattingStats(data) {
 function parseFieldingStats(data) {
 	const $ = cheerio.load(data);
 	let fieldingInfo = '\n**Fielding**';
-	const seasonYear = 2054;
+	const seasonYear = 2055;
 	const set = $(`a[href*="team_year"]:contains(${seasonYear}):contains(- MLB)`, data).parent().parent();
 	const setMinors = $(`a[href*="team_year"]:contains(${seasonYear}):contains(- R)`, data).parent().parent();
 	for (let i = 0; i < set.length; i++) {
