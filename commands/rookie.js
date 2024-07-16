3ee78bb20749fd816e439df314841fc04dbc917e
majesiu <pawel.majewski@mail.com>
2024-07-16 16:13:38 +0200

    new rookie command

---
+++ commands/rookie.js
@@ -0,0 +1,34 @@
+/**
+* MIT License
+*
+* Copyright (c) 2024 Paweł Majewski
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the 'Software'), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+
+module.exports = {
+	name: 'rookie',
+	aliases: ['newplayer'],
+	description: 'Returns link to the rookie discord',
+	cooldown: 5,
+	execute(message) {
+		message.channel.send('Join our rookie discord and find out everything you need to know about creating your future HOFer today! https://discord.gg/aTWaWv3K4J');
+	},
+};
